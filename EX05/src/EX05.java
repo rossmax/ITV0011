62a2f13274d61ccc8ddfc1676e62ccbb63b9a9b2
rossmax <rossmax@live.com>
2016-03-04 18:49:16 +0200

    123

--- EX05/src/EX05.java
+++ EX05/src/EX05.java
@@ -1,95 +1,103 @@
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 
+import java.util.Scanner;
 
 public class EX05 {
-	public static final String INPUTFile = "Movies.txt";
-	public static final String OUTPUTFile = "OutputMovies.txt";
-	public static String MOVIESList = "";
-	
-	
-    public static void main(String[] args) throws IOException {
-        System.out.println(getNicelyFormattedMovie("tere|")); // null
-    	convert(INPUTFile,OUTPUTFile);
-        System.out.println(getNicelyFormattedMovie("2016-02-24|Movie1|description|8.0"));
-        System.out.println(getNicelyFormattedMovie(MOVIESList));
-        /*
-Movie1
-Release date: 24/02/2016
-Description: description
-Average rating: 8.0    <- no new line in the end
-         */
-    }
+    public static final String INPUTFile  = "Movies.txt";
+    public static final String OUTPUTFile = "OutputMovies.txt";
+    public static String       MOVIESList = "";
+
     public static int convert(String inputFile, String outputFile) throws IOException {
-    	
-    	String ret = "";
-    	int c= 0;
-		Path path = Paths.get(inputFile);
-		Scanner scanner = new Scanner(path);
-		while (scanner.hasNextLine()) {
-			// "\n" -> newline
-			ret += scanner.nextLine() + "\n";
-			c++;
-		}
-		scanner.close();		
-		MOVIESList += ret;
-		
-		
-		FileWriter writer2 = new FileWriter(outputFile);
-		writer2.write("" + getNicelyFormattedMovie(MOVIESList));
-		writer2.close();
-		
+        String  ret     = "";
+        int     c       = 0;
+        Path    path    = Paths.get(inputFile);
+        Scanner scanner = new Scanner(path);
+
+        while (scanner.hasNextLine()) {
+
+            // "\n" -> newline
+            ret += scanner.nextLine() + "\n";
+            c++;
+        }
+
+        scanner.close();
+        MOVIESList += ret;
+
+        FileWriter writer2 = new FileWriter(outputFile);
+
+        writer2.write("" + getNicelyFormattedMovie(MOVIESList));
+        writer2.close();
+
         return c;
     }
- 
-    public static String getNicelyFormattedMovie(String movieLine) throws IOException{
-    	
-    	
-    	
-    	String ret = "";
-    	movieLine = movieLine.replaceAll("\n" , "|");
-    	String[] retAll = movieLine.split("\\|");
-    	
-    	if(retAll.length < 3)return null;
-    	
-    	for(int j = 0; j < retAll.length; j+=4){
-    		String saveLine;
-    		saveLine = retAll[j];
-    		retAll[j] = retAll[j + 1];
-    		retAll[j + 1] = saveLine;	
-    	}
-    	
-    	int name = 1,data = 0,desc = 2,rat = 3;
-    	for(int i = 0; i < retAll.length; i++){
-    		if(i == name){
-    			ret += "Release Date: " + retAll[i] + "\n";
-    			name += 4;
-    		}
-    		if(i == data){
-    			ret += retAll[i] + "\n";
-    			data += 4;
-    		}
-    		
-    		if(i == desc){
-    			ret += "Description: " + retAll[i] + "\n";
-    			desc += 4;
-    		}
-    		if(i == rat) {
-    			ret += "Average Rating: " + retAll[i] + "\n\n"; 
-    			rat += 4;
-    		}
-    	}
-		
-        
-        return ret;
-        
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getNicelyFormattedMovie("tere|"));    // null
+        convert(INPUTFile, OUTPUTFile);
+        System.out.println(getNicelyFormattedMovie("2016-02-24|Movie1|description|8.0"));
+        System.out.println(getNicelyFormattedMovie(MOVIESList));
+
+        /*
+         * Movie1
+         * Release date: 24/02/2016
+         * Description: description
+         * Average rating: 8.0    <- no new line in the end
+         */
     }
-    
-    
+
+    public static String getNicelyFormattedMovie(String movieLine) throws IOException {
+        String ret = "";
+
+        movieLine = movieLine.replaceAll("\n", "|");
+
+        String[] retAll = movieLine.split("\\|");
+
+        if (retAll.length < 3) {
+            return null;
+        }
+
+        for (int j = 0; j < retAll.length; j += 4) {
+            String saveLine;
+
+            saveLine      = retAll[j];
+            retAll[j]     = retAll[j + 1];
+            retAll[j + 1] = saveLine;
+        }
+
+        int name = 1,
+            data = 0,
+            desc = 2,
+            rat  = 3;
+
+        for (int i = 0; i < retAll.length; i++) {
+            if (i == name) {
+                ret  += "Release Date: " + retAll[i] + "\n";
+                name += 4;
+            }
+
+            if (i == data) {
+                ret  += retAll[i] + "\n";
+                data += 4;
+            }
+
+            if (i == desc) {
+                ret  += "Description: " + retAll[i] + "\n";
+                desc += 4;
+            }
+
+            if (i == rat) {
+                ret += "Average Rating: " + retAll[i] + "\n\n";
+                rat += 4;
+            }
+        }
+
+        return ret;
+    }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
