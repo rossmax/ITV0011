5a8e25b2508f181d69ad9cb59657eef400e78227
rossmax <rossmax@live.com>
2016-05-13 18:04:01 +0300

    EX19 lahendus

--- EX19/src/EX19.java
+++ EX19/src/EX19.java
@@ -32,8 +32,8 @@ public class EX19 {
     public static int[] testDog(String text) {
 
 
-        if (text.equals("") || text == "") {
-            return null;
+        if (text.equals("") || text == ""|| text == null) {
+            return new int[]{};
         }
             int elusKoer = 0;
             int surnudKoer = 0;
