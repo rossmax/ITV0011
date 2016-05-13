0dca1abbd2b376c71fff957bbd5d2f0b14c532d7
rossmax <rossmax@live.com>
2016-05-13 17:44:11 +0300

    EX19 lahendus

--- EX19/src/EX19.java
+++ EX19/src/EX19.java
@@ -35,7 +35,7 @@ public class EX19 {
                 return null;
             }
             if (text.equals("")) {
-                return new int[]{0};
+                return null;
             }
             int elusKoer = 0;
             int surnudKoer = 0;
